0ea754489f79647637934495d07f07e03e4aba64
Keon <mr.edwards.kj@gmail.com>
2023-02-04 00:20:06 +0000

    updated comments in file

--- awsserviceinventory.py
+++ awsserviceinventory.py
@@ -3,11 +3,13 @@
 
 # I will be creating a list of AWS services and populating it into an empty list
 
-# The list will start out empty
+# Start with an empty list
 
 services = []
 print(services)
-# I will now populate the services list with services from AWS
+
+
+# Populate the services list with services from AWS using append
 
 services.append("S3")
 services.append("X-Ray")
@@ -16,15 +18,16 @@ services.append("Beanstalk")
 services.append("IAM")
 services.append("LAMBDA")
 
-# Now I will print out the list and length of the list
+# Print the list and length of the list
 
 print(services)
 print("There are", len(services), "services on this list")
 
-# Remove two specific services from the list
+# Remove two specific services from the list by index
 
 del services[2:4]
 
 # Print new list and length of list
 
-print("The new list of services", services, "with a quantity of", len(services), "services.")
+print(services)
+print("There are now ", len(services), "services in the list.")
