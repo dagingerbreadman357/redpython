c940b688f65b521bb750f252e02c7d55f09c297e
Keon <mr.edwards.kj@gmail.com>
2023-02-07 15:10:41 +0000

    added comments to file

--- code/while-loop-practice.py
+++ code/while-loop-practice.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+
+# while CONDITIONS:
+#    print("Something")
+
+# This will be an infinite loop because 1 will always be less than 2
+# the condition in the loop needs to be dynamic if we want to stop the loop
+
+#  while 1 < 2:
+#      print('something')
+
+count = 1
+
+while count <= 4:
+    print("looping")
+    count += 1
+    
