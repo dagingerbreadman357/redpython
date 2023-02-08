8c5bb17e83e57c441b3e8a77b8d49671f6a3f379
Keon <mr.edwards.kj@gmail.com>
2023-02-08 22:57:36 +0000

    functions basics lesson

---
+++ code/functions-basics.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# defining a function by using def
+
+# def print_name(name)
+#     print(f"Name is {name})
+
+def print_name(name):
+    print(f"Name is {name}")
+
+print_name("Keon")
+
+def high_school(hs):
+    print(f"High School name is {hs}") 
+high_school("Wando")
+
+def add_two(num):
+    return num + 2
+
+result = add_two(6)
+    
+print(result)
+    
