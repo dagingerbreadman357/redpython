a3912de6ddd41962d4fa215be19c1003b8720f79
Keon <mr.edwards.kj@gmail.com>
2023-02-10 01:12:21 +0000

    updated script

--- list-script.py
+++ list-script.py
@@ -12,6 +12,6 @@ dir_list = list(os.listdir(path))
 for path in os.listdir():
     dict_list = {}                   
     dict_list['path'] = os.path.realpath(path)
-    file_size = os.stat(path) 
+    file_size = os.stat(path)
     dict_list['size'] = file_size.st_size
     print(dict_list)
